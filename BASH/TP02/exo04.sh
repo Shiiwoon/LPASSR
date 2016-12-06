45b507b0490c3a63c978c9d9afa10e166e3ab5f4
Jorane Congio <jorane.congio@gmail.com>
2016-12-06 16:53:25 +0100

    Le tri c'est la vie

--- BASH/TP02/exo04.sh
+++ BASH/TP02/exo04.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
 
+## Pour alléger la fonction tri, on fai un fonction qui échange les éléments à côté
+function swap {
+	tmp=${tableau[$1]}
+	tableau[$1]=${tableau[$2]}
+	tableau[$2]=$tmp
+}
+
+## Trie le tableau selon l'algorithme de tri par sélection
 function tri {
-	while [ $i < ${#$1[*]} ]; do
-		echo $i
-		i++
+	len_tab=${#tableau[@]}
+	for ((i=0 ; i < len_tab ; i++)); do
+		min=$i
+		for ((j = i+1 ; j < len_tab ; j++)); do
+			if [ ${tableau[j]} -lt ${tableau[min]} ]; then
+				min=$j
+			fi
+		done
+		swap $i $min
 	done
 }
 
-read -p "Saisir un tableau d'entier : " tableau
-echo $tableau
-#tableau_trie=$(echo ${tableau[*]} | tr ' ' '\n' | sort)
-#echo $tableau_trie
+## Programme principal
+echo -n "Saisir un tableau d'entier : "
+read -a tableau
 tri $tableau
+echo "tableau : ${tableau[*]}"
 echo "Note minimale : ${tableau[0]}"
+echo "Note maximale : ${tableau[-1]}"
